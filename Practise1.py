f53dfd31805fcab753c584a0d75056aacbf64a16
LiverpoolFC314 <gareshsingh794@gmail.com>
2026-02-26 17:50:54 +0000

    Add files via upload

---
+++ Practise1.py
@@ -0,0 +1,9 @@
+"""Write a Python program that asks the user for their first name and last name and print the output in the following format. 
+Example:
+Enter your first name: Regina 
+Enter your last name: Phalange
+Phalange’s first name is Regina"""
+firstname=input("Enter your first name")
+surname=input("Enter your surname")
+print("Hello my name is",firstname,surname)
+print(surname,"'s first name is",firstname)
---
+++ Practise2.py
@@ -0,0 +1,12 @@
+"""Write a Python program that asks the user for their name and their favourite colour, and print the output in the following format. 
+Example:
+Enter your name: Maxi
+Enter your favourite colour: pink
+Hey Maxi, Your favourite colour is pink.
+Example:
+Enter your name: Maxi
+Enter your favourite colour: pink
+Hey Maxi, Your favourite colour is pink."""
+name=input("Enter your name")
+favoritecolor=input("Enter your favourite color")
+print("Hey",name,"your favorite color is ",favoritecolor)
