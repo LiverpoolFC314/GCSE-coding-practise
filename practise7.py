4b6172e9a10e31782f93c243ca48436f6a3c7b1e
LiverpoolFC314 <gareshsingh794@gmail.com>
2026-05-14 17:53:53 +0100

    Create practise7.py

---
+++ practise7.py
@@ -0,0 +1,79 @@
+"""Create a project to ask the user his name and print it 6 times using for loop"""
+# name=(input("Please enter your name"))
+    
+# for i in range(6):
+#     print("Please enter",name)
+# count=0 
+# while count<6:
+#     print("Coding is cool")
+#     count=count+1
+    
+"""Create a project to ask the user his name and print it 6 times using while loop."""
+# name=(input("Please enter your name"))
+# count=0
+# while count<6:
+#  print(name)
+#  count=count+1
+"""Create a project to ask the user his name and print it 6 times using for loop"""
+# name=(input("Please enter your name"))
+    
+# for i in range(6):
+#     print("Please enter",name)
+
+
+"""Create a project to print all the numbers from 0 to 10 using for loop."""
+# for i in range(0,11):
+#     print(i)
+# count=0
+# while count<11:
+#    print(count)
+#    count=count+1
+
+
+
+"""Create a project to print all the numbers from 7 to 15 using for loop."""
+# for i in range(7,16):
+#     print(i)
+# count=7
+# while count<16:
+#    print(count)
+#    count=count+1
+
+"""Create a project to print all the even numbers from 6 to 20 using for loop."""
+# for i in range(6,21,2):
+#  print(i)
+# count=6
+# while count<21:
+#    print(count)
+#    count=count+2
+
+"""Create a project to print all the odd numbers from 7 to 15 using for loop."""
+# for i in range(7,16,2):
+    # print(i)
+# count=7
+# while count<16:
+#    print(count)
+#    count=count+2
+"""Create a project to print ten multiples of 3 using for loop."""
+# for i in range(3,31,3):
+#     print(i)
+# count=3
+# while count<31:
+#     print(count)
+#     count=count+3
+
+"""Create a project to print twelve multiples of 10 using for loop."""
+# for i in range(10,121,10):
+#     print(i)
+count=10
+while count<121:
+    print(count)
+    count=count+10
+"""Create a project to print all the numbers from 10 to 1 using for loop."""
+# for i in range(10,0,-1):
+#     print(i)
+
+count=10
+while count>0:
+    print(count)
+    count=count-1
