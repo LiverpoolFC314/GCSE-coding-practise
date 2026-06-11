88f8a5e00ed8e2f78e6b9dc2e101de493ef08aff
LiverpoolFC314 <gareshsingh794@gmail.com>
2026-06-11 17:57:30 +0100

    Add files via upload

---
+++ practise8.py
@@ -0,0 +1,22 @@
+
+# Inputlist=[]
+# for i in range(10):
+#     num = int(input("Enter a number"))
+#     Inputlist.append(num)
+    
+# sum=0
+# for num in Inputlist:
+#     sum=sum+num 
+
+# print("The sum of the number in sum",sum)
+
+list1=[1,2,3,4,5,6,7,8,9,10,11]
+# length=len(list1)
+# if length ==0:
+#     print("The list is empty")
+# else:
+#     print("The list is not empty")
+
+for i in range(0,11):
+    if list1[i]%2==1:
+        print(list1[i])
